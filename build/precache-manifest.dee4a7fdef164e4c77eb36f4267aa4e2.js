45973867241ce6cce23ffb8937f92897de653e9a
SamuelSanthosh <samuelsanthosh1997@gmail.com>
2020-02-02 00:28:54 +0530

    updated commit

---
+++ build/precache-manifest.dee4a7fdef164e4c77eb36f4267aa4e2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8cdeb9bed6708b685918625749f60e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ace38169931a29a6001d",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "efe39c97b511d5e46c93",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ace38169931a29a6001d",
+    "url": "/static/js/2.57e416de.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.57e416de.chunk.js.LICENSE"
+  },
+  {
+    "revision": "efe39c97b511d5e46c93",
+    "url": "/static/js/main.6cdcc2f8.chunk.js"
+  },
+  {
+    "revision": "8831e80c65ffde5ff66b",
+    "url": "/static/js/runtime-main.0a8b9576.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
